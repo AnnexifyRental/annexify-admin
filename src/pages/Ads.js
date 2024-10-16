31766de71331173afbc6a858a789c44f5c57567f
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-10-17 01:35:55 +0530

    Feature: Initial Setup done. Navbar Sidebar Routing

---
+++ src/pages/Ads.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function Ads() {
+  return (
+    <div>
+      <Typography variant="h4">Ads</Typography>
+      <Typography>Manage and verify ads here.</Typography>
+    </div>
+  );
+}
+
+export default Ads;
